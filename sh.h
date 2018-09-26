b40fc13261a15319a1704a124d61c2a408756f6d
Ryan Geary <rtgnj42@gmail.com>
2018-09-26 00:01:38 -0400

    Update .h

--- sh.h
+++ sh.h
@@ -3,7 +3,7 @@
 
 int pid;
 int sh( int argc, char **argv, char **envp);
-char* cd(char **args, char* homedir, char* prevDir, char* pwd);
+void cd(char *args, char* homedir, char* prevDir, char* pwd);
 char *which(char *command, struct pathelement *pathlist);
 char *where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
