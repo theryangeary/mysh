56853e4a9c0571a3ecaaee8da04d58d1f992e0b5
Ryan Geary <rtgnj42@gmail.com>
2018-09-27 15:02:06 -0400

    Handle signals

--- main.c
+++ main.c
@@ -7,12 +7,24 @@ void sig_handler(int signal);
 int main( int argc, char **argv, char **envp )
 {
   /* put signal set up stuff here */
-
+  if (signal(SIGINT, sig_handler) == SIG_ERR)
+    printf("\ncan't catch SIGINT\n");
+  else if (signal(SIGTERM, sig_handler) == SIG_ERR) {
+    printf("\ncan't catch SIGTERM");
+  }
+  else if (signal(SIGTSTP, sig_handler) == SIG_ERR) {
+    printf("\ncan't catch SIGTSTP");
+  }
   return sh(argc, argv, envp);
 }
 
 void sig_handler(int signal)
 {
   /* define your signal handler */
+  if (SIGINT == signal) {
+    printf("\n");
+  }
+  else if (SIGTERM == signal || SIGTSTP == signal) {
+  }
 }
 
