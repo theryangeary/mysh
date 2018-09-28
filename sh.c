e05a2892fa20d8590b76af21a5c8a00934e2073c
Ryan Geary <rtgnj42@gmail.com>
2018-09-28 11:26:33 -0400

    Free history

--- sh.c
+++ sh.c
@@ -279,6 +279,11 @@ int sh( int argc, char **argv, char **envp )
         fprintf(stderr, "%s: Command not found.\n", command);
     }
   }
+  do {
+    struct historyelement* prev = lastcommand->prev;
+    free(lastcommand);
+    lastcommand = prev;
+  } while(lastcommand->prev);
   free(prompt);
   free(commandline);
   free(owd);
